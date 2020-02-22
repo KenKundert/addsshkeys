74bb94ce402362f8f4eafdb4364ac04c5797fa21
Ken Kundert <ken@theKunderts.net>
2020-02-22 00:01:26 -0800

    normalize code (ran isort, black, flake8)

--- setup.py
+++ setup.py
@@ -1,39 +1,41 @@
 #!/usr/bin/env python
 
-from setuptools import setup
 from codecs import open
 
-with open('README.rst', encoding='utf-8') as f:
+from setuptools import setup
+
+with open("README.rst", encoding="utf-8") as f:
     readme = f.read()
 
 setup(
-    name = 'addsshkeys',
-    version = '0.0.2',
-    author = 'Ken Kundert',
-    author_email = 'addsshkeys@nurdletech.com',
-    description = 'Add keys to SSH Agent',
-    long_description = readme,
-    url = 'https://github.com/kenkundert/addsshkeys',
-    download_url = 'https://github.com/kenkundert/addsshkeys/tarball/master',
-    license = 'GPLv3+',
-    scripts = 'addsshkeys'.split(),
-    install_requires = [
-        'appdirs',
-        'avendesora>=1.12',
-        'docopt',
-        'inform>=1.14',
+    name="addsshkeys",
+    version="0.0.2",
+    author="Ken Kundert",
+    author_email="addsshkeys@nurdletech.com",
+    description="Add keys to SSH Agent",
+    long_description=readme,
+    url="https://github.com/kenkundert/addsshkeys",
+    download_url="https://github.com/kenkundert/addsshkeys/tarball/master",
+    license="GPLv3+",
+    scripts="addsshkeys".split(),
+    install_requires=[
+        "appdirs",
+        "avendesora>=1.12",
+        "docopt",
+        "inform>=1.14",
+        "pexpect",
     ],
-    python_requires='>=3.6',
-    keywords = 'avendesora ssh'.split(),
-    classifiers = [
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Console',
-        'Intended Audience :: End Users/Desktop',
-        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Utilities',
+    python_requires=">=3.6",
+    keywords="avendesora ssh".split(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Utilities",
     ],
 )
